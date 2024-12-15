648bf57a6f5df3d414cb4a6cc3c52cabc762e943
kartikey khade <kartikey.khade1995@gmail.com>
2024-12-15 11:40:24 +0530

    Commit

--- src/test/java/demo/Class1.java
+++ src/test/java/demo/Class1.java
@@ -15,49 +15,55 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Class1 {
-    public static void main(String args[]) throws IOException, InterruptedException {
-       
-    	// Load configuration properties from a file
-        Properties prop = new Properties();
-        FileInputStream fis = new FileInputStream("./Configuration\\config.properties");
-        prop.load(fis);
-        String bName = prop.getProperty("chromepath");
-        
-        // Set the path for the ChromeDriver reading from config file
-        System.setProperty("webdriver.chrome.driver", bName);
-        
-        // Initialize WebDriver
-        ChromeOptions co = new ChromeOptions();
-        WebDriver driver = new ChromeDriver(co);
-        driver.manage().window().maximize();
-        driver.get("https://www.amazon.in");
+	public static void main(String args[]) throws IOException, InterruptedException {
 
-        // Set implicit wait globally (applies to all element lookups)
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Global implicit wait for 10 seconds
-        System.out.println("Page launched");
+		// Load configuration properties from a file
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream("./Configuration\\config.properties");
+		prop.load(fis);
+		String bName = prop.getProperty("chromepath");
 
-        // Initialize JavaScript Executor
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+		// Set the path for the ChromeDriver reading from config file
+		System.setProperty("webdriver.chrome.driver", bName);
 
-        // Explicit wait for the menu to be clickable before clicking
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Explicit wait with Duration
-        WebElement menu = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/deals?ref_=nav_cs_gb']")));
-        js.executeScript("arguments[0].click();", menu);  // Using JavaScriptExecutor for the click
-        System.out.println("Menu clicked");
+		// Initialize WebDriver
+		ChromeOptions co = new ChromeOptions();
+		WebDriver driver = new ChromeDriver(co);
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in");
 
-        // Explicit wait for the deal link to be clickable before clicking
-        WebElement deal = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='GridRow-module__container_q6XsDi4clqdE6jhYFSBW']/div[1]//a")));
-        deal.click();
-        System.out.println("Deal clicked");
+		// Set implicit wait globally (applies to all element lookups)
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Global implicit wait for 10 seconds
+		System.out.println("Page launched");
 
-        // Explicit wait for the 'Add to Cart' button to be clickable before clicking
-        js.executeScript("window.scrollBy(0, 200);");
-        WebElement addCartBtn = driver.findElement(By.xpath("//span[@id='submit.add-to-cart']//input"));
-        js.executeScript("arguments[0].click();", addCartBtn);  // Using JavaScriptExecutor for the click
-        System.out.println("Add to Cart clicked");
+		// Initialize JavaScript Executor
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        // Close the browser
-        System.out.println("Closing the browser");
-        driver.quit();
-    }
+		
+		
+		
+		//asdf
+		// Explicit wait for the menu to be clickable before clicking
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Explicit wait with Duration
+		WebElement menu = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/deals?ref_=nav_cs_gb']")));
+		js.executeScript("arguments[0].click();", menu); // Using JavaScriptExecutor for the click
+		System.out.println("Menu clicked");
+
+		// Explicit wait for the deal link to be clickable before clicking
+		WebElement deal = wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//div[@class='GridRow-module__container_q6XsDi4clqdE6jhYFSBW']/div[1]//a")));
+		deal.click();
+		System.out.println("Deal clicked");
+
+		// Explicit wait for the 'Add to Cart' button to be clickable before clicking
+		js.executeScript("window.scrollBy(0, 200);");
+		WebElement addCartBtn = driver.findElement(By.xpath("//span[@id='submit.add-to-cart']//input"));
+		js.executeScript("arguments[0].click();", addCartBtn); // Using JavaScriptExecutor for the click
+		System.out.println("Add to Cart clicked");
+
+		// Close the browser
+		System.out.println("Closing the browser");
+		driver.quit();
+	}
 }
